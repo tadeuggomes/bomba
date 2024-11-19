511023f2719e9c4a8dacbbeb380340174490fd29
Maria Júlia <mjmagalhaes2204@gmail.com>
2024-11-18 21:51:13 -0300

    img

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ alterarCor()
 
 function tamanhoImagem(tamanhoImagem){
 
-    const imagens = document.querySelectorAll(".image-container img");
+    const imagens = document.querySelectorAll(".imagem-container img");
 
     imagens.forEach(imagem => {
         imagem.addEventListener("mouseover", function() {
